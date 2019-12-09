b831ba77943b543c3d072745c1c3a8265a711dc9
Jarno Ralli <jarno@ralli.fi>
2019-12-09 06:47:08 +0200

    * Builds with Visual Studio 2017 (Tested in Windows 10, 64-bit) * Builds with gcc 7.4.0 (Tested in Linux Mint 19 Tara) * Completely new structure, e.g. include file structure mimics namespace structure * Uses modern CMake approach * Exports targets (not tested properly) * Uses FetchContent to get most of the 3rd party libraries * Pangolin viewer tested in both Linux and Windows * More testing needed

---
+++ test/util/src/test_trigonometric.cpp
@@ -0,0 +1,20 @@
+#include "openvslam/type/type.hpp"
+#include "openvslam/util/trigonometric.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace openvslam;
+
+TEST(trigonometric, cos) {
+    for (unsigned int deg = 0; deg <= 3600; ++deg) {
+        const float rad = (deg / 10.0f) * M_PI / 180.0f;
+        EXPECT_NEAR(std::cos(rad), util::cos(rad), 1e-3);
+    }
+}
+
+TEST(trigonometric, sin) {
+    for (unsigned int deg = 0; deg <= 3600; ++deg) {
+        const float rad = (deg / 10.0f) * M_PI / 180.0f;
+        EXPECT_NEAR(std::sin(rad), util::sin(rad), 1e-3);
+    }
+}
